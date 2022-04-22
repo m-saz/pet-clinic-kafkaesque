06c7e46ea2b3059d33ddc8b0418e123945039e1c
hyh4x <everhax@gmail.com>
2022-04-22 23:13:28 +0300

    Added CrossOrigin

--- petclinic/src/main/java/no/group/petclinic/controller/OwnerController.java
+++ petclinic/src/main/java/no/group/petclinic/controller/OwnerController.java
@@ -2,6 +2,7 @@ package no.group.petclinic.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import no.group.petclinic.service.OwnerService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/owners")
+@CrossOrigin(origins = "http://localhost:4200")
 public class OwnerController {
 	
 	private final OwnerService ownerService;
--- petclinic/src/main/java/no/group/petclinic/controller/VetController.java
+++ petclinic/src/main/java/no/group/petclinic/controller/VetController.java
@@ -2,6 +2,7 @@ package no.group.petclinic.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import no.group.petclinic.service.VetService;
 @RestController
 @RequestMapping("/api/vets")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class VetController {
 	
 	private final VetService vetService;
