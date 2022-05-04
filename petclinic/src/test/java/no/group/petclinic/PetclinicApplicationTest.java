da6e446db6ac92cb04c2d6e1d2a5333808879a36
hyh4x <everhax@gmail.com>
2022-05-05 01:31:04 +0300

    fixed test application context loading

---
+++ petclinic/src/test/java/no/group/petclinic/PetclinicApplicationTest.java
@@ -0,0 +1,15 @@
+package no.group.petclinic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
